4a9d1229fa96bb1b8dcd08d44ca4625a73fdf7e9
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-21 19:31:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,9 +8,9 @@ class Application
     num_3 = Kernel.rand(1..20)
 
     if num_1 == num_2 && num_1 == num_3
-      resp.write "You won!"
+      resp.write "You Win"
     else
-      resp.write "You lost"
+      resp.write "You Lose"
     end
     resp.finish
   end
